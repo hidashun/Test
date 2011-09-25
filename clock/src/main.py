e6528ee07dca9d1ae2da1d328e3c5ff4f9b7c67f
shunsuke <h.shunsuke@pg7.so-net.ne.jp>
2011-09-25 15:18:36 +0900

    タイムゾーン設定機能追加

--- clock/src/main.py
+++ clock/src/main.py
@@ -2,6 +2,7 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import datetime
+import models
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -13,13 +14,27 @@ class MainPage(webapp.RequestHandler):
                          users.create_login_url(self.request.path)
                          )
                       )
+            tz_form = ''
         else:
+            userprefs = models.get_userprefs()
             navbar = ('<p>Welcome, %s ! You can <a href="%s">sign out</a>.</p>'
                       % (
-                         user.email(),
+                         user.nickname(),
                          users.create_logout_url(self.request.path)
                          )
                       )
+            tz_form = '''
+                <form action="/prefs" method="post">
+                    <label for="tz_offset">
+                        Timezone offset from UTC (can be negative):
+                    </label>
+                    <input name="tz_offset" id="tz_offset" type="text"
+                        size="4" value="%d" />
+                    <input type="submit" value="Set" />
+                </form>
+            ''' % userprefs.tz_offset
+            time += datetime.timedelta(0, 0, 0, 0, 0, userprefs.tz_offset)
+        
         self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write('''
         <html>
@@ -29,9 +44,10 @@ class MainPage(webapp.RequestHandler):
             <body>
             %s
                 <p>The time is: %s</p>
+            %s
             </body>
         </html>
-        ''' % (navbar, str(time)))
+        ''' % (navbar, str(time), tz_form))
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
