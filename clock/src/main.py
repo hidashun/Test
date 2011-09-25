aa43afed35acf792b2bed860c58feb9bf3b415fe
shunsuke <h.shunsuke@pg7.so-net.ne.jp>
2011-09-25 14:01:10 +0900

    ログイン状態によって変化

--- clock/src/main.py
+++ clock/src/main.py
@@ -1,3 +1,4 @@
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import datetime
@@ -5,8 +6,32 @@ import datetime
 class MainPage(webapp.RequestHandler):
     def get(self):
         time = datetime.datetime.now()
+        user = users.get_current_user()
+        if not user:
+            navbar = ('<p>Welcome! <a href="%s">Sign in or register</a> to customize.</p>'
+                      % (
+                         users.create_login_url(self.request.path)
+                         )
+                      )
+        else:
+            navbar = ('<p>Welcome, %s ! You can <a href="%s">sign out</a>.</p>'
+                      % (
+                         user.email(),
+                         users.create_logout_url(self.request.path)
+                         )
+                      )
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write('<p>The time is: %s</p>' % str(time))
+        self.response.out.write('''
+        <html>
+            <head>
+                <title>The Time Is...</title>
+            </head>
+            <body>
+            %s
+                <p>The time is: %s</p>
+            </body>
+        </html>
+        ''' % (navbar, str(time)))
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
